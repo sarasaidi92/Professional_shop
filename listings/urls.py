57482b09aad4369dc698259931d0b353438dc1d3
sarasaidi92 <160146978+sarasaidi92@users.noreply.github.com>
2025-04-27 11:51:21 +0330

    create users app

--- listings/urls.py
+++ listings/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from  .views import listings
+from  .views import listings, listing_detail
 
 urlpatterns = [
     path('', listings, name='listings'),
+    path('detail/', listing_detail, name='listing_detail'),
 
 ]
--- listings/views.py
+++ listings/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 def listings(request):
     return render(request, 'listings.html')
 
+def listing_detail(request):
+    return render(request, 'listing_detail.html')
